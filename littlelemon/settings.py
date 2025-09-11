b96a97cf9958e5a83791faf93b885504aa58ff26
Matheus <matheus.figueiredo0620@gmail.com>
2025-09-10 23:41:39 -0300

    add: Assets Folders (templates+static)

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -55,10 +55,11 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
