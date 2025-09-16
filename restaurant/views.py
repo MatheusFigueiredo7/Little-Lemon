b1ce6e0d44395819bd19c44239f4b85c03f67813
Matheus <matheus.figueiredo0620@gmail.com>
2025-09-16 01:12:29 -0300

    add: MenuItem + SingleMenuItem views and serializers

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .serializers import MenuItemSerializer
+from .models import MenuItem
+from rest_framework import generics
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
