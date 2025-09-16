93523dfaaf115707e3b11fd3c55586c9842d00d4
Matheus <matheus.figueiredo0620@gmail.com>
2025-09-16 16:10:35 -0300

    add: Djoser + Security Features

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import MenuItemsView, SingleMenuItemView
+from django.urls import path, include
+from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'bookings', views.BookingViewSet, basename='booking')
 
 urlpatterns = [
-    path('menu/', MenuItemsView.as_view()),
-    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('message/', views.msg),
+    path('api-token-auth/', obtain_auth_token),
+    path('', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,19 +1,23 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from .serializers import MenuItemSerializer, BookingSerializer
 from .models import MenuItem, Booking
-from rest_framework import generics, viewsets, permissions
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view()
+def msg(request):
+    return Response({"message": "This view is protected"})
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [permissions.IsAuthenticated] 
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
-    permission_classes = [permissions.IsAuthenticated] 
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated] 
