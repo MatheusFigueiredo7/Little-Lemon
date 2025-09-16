0ea1fa6da85d45111420dfb354e6c2e2f211e4dc
Matheus <matheus.figueiredo0620@gmail.com>
2025-09-16 01:46:57 -0300

    add: BookingViewSet

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,12 +1,19 @@
 from rest_framework.decorators import api_view
-from .serializers import MenuItemSerializer
-from .models import MenuItem
-from rest_framework import generics
+from .serializers import MenuItemSerializer, BookingSerializer
+from .models import MenuItem, Booking
+from rest_framework import generics, viewsets, permissions
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated] 
 
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
